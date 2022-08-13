e2e4223f6e4adb66b0e9a72fa22c1329f5cc775b
AnlunZhang <zhanganlun_okk@163.com>
2022-08-13 14:36:39 +0800

    feat: add HTML template

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,7 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld-sample" is now active!');
+	console.log('Congratulations, your extension "allToTS-sample" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -16,8 +16,52 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+		// vscode.window.showInformationMessage('Hello !');
+		const panel = vscode.window.createWebviewPanel(
+			'sourceFile', // Identifies the type of the webview. Used internally
+			'Source File', // Title of the panel displayed to the user
+			vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+			{} // Webview options. More on these later.
+		  );
+		  panel.webview.html = `<!DOCTYPE html>
+		  <html lang="en">
+		  <head>
+			  <meta charset="UTF-8">
+			  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+			  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+			  <title>Source File</title>
+		  </head>
+		  <style>
+				.textarea-wrapper{
+					width: 300px;
+					height: 100vh;
+				}
+				.json-source{
+					padding:10px;
+					border:1px solide #eee
+					width:100%
+					height:80vh
+					display:block
+					resize:none
+				}
 
+		  </style>
+		  <body>
+			  <div class="textarea-wrapper">
+				<textarea class="json-source">
+				</textarea>
+			  </div>
+			  <button id="action-getts">get ts</button>
+			  <script>
+
+				const $getTsElem = document.getElementById("action-getts");
+				$getTsElem && $getTsElem.addEventListener('click', function() {
+					console.log(1)
+
+				})
+			  </script>
+		  </body>
+		  </html>`
+	});
 	context.subscriptions.push(disposable);
 }
